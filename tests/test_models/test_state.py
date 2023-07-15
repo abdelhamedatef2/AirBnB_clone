a8693936fe9e4fbb2066598682f3b31fd7b8187e
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-07-15 19:41:09 +0300

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,16 +1,41 @@
 #!/usr/bin/python3
-""" test State """
+""" module to test state class """
 import unittest
-import pep8
+from models import state
 from models.state import State
 
-class State_testing(unittest.TestCase):
-    """ check BaseModel class"""
 
-    def testpep8(self):
-        """ test codestyle """
-        pepstylecode = pep8.StyleGuide(quiet=True)
-        path_user = 'models/state.py'
-        result = pepstylecode.check_files([path_user])
-        self.assertEqual(result.total_errors, 0,
-                         "if found code style error")
+class TestState(unittest.TestCase):
+    """ class that test state module and state class """
+
+    def test_module_doc(self):
+        """ method test if there doc for module """
+        doc = len(state.__doc__.strip())
+        self.assertTrue(doc > 0)
+
+    def test_class_doc(self):
+        """ method test if there is doc for class """
+        doc = len(State.__doc__.strip())
+        self.assertTrue(doc > 0)
+
+    def test_empty_attribute(self):
+        """ method test if has class attributes empty """
+        new = State()
+        self.assertTrue(hasattr(new, "name"))
+        self.assertTrue(len(new.name) == 0)
+
+    def test_type_attributes(self):
+        """ method test type of value of attributes """
+        new = State()
+        self.assertIs(type(new.name), str)
+
+    def test_modify_attributes(self):
+        """ method test of modified state """
+        new = State()
+        new.name = "cairo"
+        self.assertEqual(new.name, "cairo")
+        self.assertEqual(State.name, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
