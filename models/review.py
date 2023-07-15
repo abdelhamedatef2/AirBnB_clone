6de1cb7158a3cdb096dc4c3b1d61d5626a58de27
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-07-15 19:05:34 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-""" Class Review """
+'''class inherent of BaseModel'''
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ inherts BaseModel """
+    '''class Review'''
+
     place_id = ""
     user_id = ""
     text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
