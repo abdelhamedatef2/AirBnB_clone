8f90081e2af9b18facd4fdb51cfcca63f4038662
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-07-15 19:34:28 +0300

    Update __init__.py

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-'''create a unique FileStorage instance for your application'''
-from models.engine.file_storage import FileStorage
-
-'''A variable storage, an instance of FileStorage'''
-storage = FileStorage()
-storage.reload()
+""" empty module th start package """
