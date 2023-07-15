ce116211ec0e9e561201774fa845b51d0129ec8b
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-07-15 19:36:26 +0300

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-'''class inherent of Base Model'''
-from models.base_model import BaseModel
+""" module that represent place model """
+from .base_model import BaseModel
 
 
 class Place(BaseModel):
-    '''class Place'''
-
+    """ class of place model inherit from BaseModel class
+        Attr:
+            city_id: public class attr id of city
+            user_id: public class attr id of user
+            name: public class attr name of state
+            description: public class attr of description of place
+            number_rooms: public class attr of number of rooms
+            number_bathrooms: public class attr number of bathroom
+            max_guest: public class attr max number of guests
+            price_by_night: public class attr price per night
+            latitude: public class attr of latitude of place
+            longitude: public class attr of longitude of place
+            amenity_ids: public class attr of list of amenity of id
+    """
     city_id = ""
     user_id = ""
     name = ""
@@ -17,7 +29,3 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
-
-    def __init__(self, *args, **kwargs):
-        """initializes Place"""
-        super().__init__(*args, **kwargs)
