43edf7cc01d566b7cf6c150bacd16d5e42b6c96f
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-07-15 19:05:50 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-""" Class State """
+'''class inherent of BaseModel'''
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """ inherits BaseModel"""
+    '''class State'''
+
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes State"""
+        super().__init__(*args, **kwargs)
