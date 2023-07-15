bc1a8ad94a26c72b85921e40234e505fe630d946
Abdel-hamed Atef <118136210+abdelhamedatef2@users.noreply.github.com>
2023-07-15 19:39:59 +0300

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,16 +1,47 @@
 #!/usr/bin/python3
-""" test city """
+""" module to test city class """
 import unittest
-import pep8
+from models import city
 from models.city import City
 
-class City_testing(unittest.TestCase):
-    """ check BaseModel class"""
 
-    def testpep8(self):
-        """ test codestyle """
-        pepstylecode = pep8.StyleGuide(quiet=True)
-        path_user = 'models/city.py'
-        result = pepstylecode.check_files([path_user])
-        self.assertEqual(result.total_errors, 0,
-                         "if found code style errors")
+class TestCity(unittest.TestCase):
+    """ class that test city module and City class """
+
+    def test_module_doc(self):
+        """ method test if there doc for module """
+        doc = len(city.__doc__.strip())
+        self.assertTrue(doc > 0)
+
+    def test_class_doc(self):
+        """ method test if there is doc for class """
+        doc = len(City.__doc__.strip())
+        self.assertTrue(doc > 0)
+
+    def test_empty_attribute(self):
+        """ method test if has class attributes empty """
+        new = City()
+        self.assertTrue(hasattr(new, "name"))
+        self.assertTrue(len(new.name) == 0)
+        self.assertTrue(hasattr(new, "state_id"))
+        self.assertTrue(len(new.state_id) == 0)
+
+    def test_type_attributes(self):
+        """ method test type of value of attributes """
+        new = City()
+        self.assertIs(type(new.name), str)
+        self.assertIs(type(new.state_id), str)
+
+    def test_modify_attributes(self):
+        """ method test of modified state """
+        new = City()
+        new.name = "cairo"
+        new.state_id = "555"
+        self.assertEqual(new.name, "cairo")
+        self.assertEqual(new.state_id, "555")
+        self.assertEqual(City.name, "")
+        self.assertEqual(City.state_id, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
